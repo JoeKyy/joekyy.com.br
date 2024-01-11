e324a3bfc9e5c68f8abf9d6708b264f7b789573d
Jhomar 'Joe' Nando <contato@joekyy.com.br>
2024-01-11 19:43:17 -0300

    fix(hello): add a scroll to next section

--- HTML/app/assets/js/jquery.functions.js
+++ HTML/app/assets/js/jquery.functions.js
@@ -1,12 +1,39 @@
 $(function () {
-    const element = document.querySelector("body");
+  // Função para verificar se o usuário está em um dispositivo móvel
+  function isMobileDevice() {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
 
-    element.addEventListener('wheel', (event) => {
-      event.preventDefault();
+  if (!isMobileDevice()) {
+      const sections = document.querySelectorAll('section');
+      let currentSectionIndex = 0;
+      let isScrolling = false;
 
-      element.scrollBy({
-        left: event.deltaY < 0 ? -30 : 30,
+      const onWheelEvent = (event) => {
+          if (isScrolling) {
+              return;
+          }
 
-      });
-    });
+          event.preventDefault();
+          isScrolling = true;
+          setTimeout(() => isScrolling = false, 1000);
+
+          if (event.deltaY < 0 && currentSectionIndex > 0) {
+              currentSectionIndex--;
+          } else if (event.deltaY > 0 && currentSectionIndex < sections.length - 1) {
+              currentSectionIndex++;
+          }
+
+          const targetSection = sections[currentSectionIndex];
+          if (targetSection) {
+              targetSection.scrollIntoView({
+                  behavior: 'smooth',
+                  block: 'nearest',
+                  inline: 'start'
+              });
+          }
+      };
+
+      document.addEventListener('wheel', onWheelEvent, { passive: false });
+  }
 });
