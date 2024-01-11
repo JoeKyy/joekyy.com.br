7909981790f2bc42d94df6b9e63d095f6c230799
Jhomar 'Joe' Nando <contato@joekyy.com.br>
2024-01-11 19:05:14 -0300

    feat(hello): finish initial structure and hello page

--- HTML/app/assets/js/jquery.functions.js
+++ HTML/app/assets/js/jquery.functions.js
@@ -1,3 +1,12 @@
 $(function () {
+    const element = document.querySelector("body");
 
+    element.addEventListener('wheel', (event) => {
+      event.preventDefault();
+
+      element.scrollBy({
+        left: event.deltaY < 0 ? -30 : 30,
+
+      });
+    });
 });
