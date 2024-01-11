f757c70864d1e939984be2f20284a7bfe62b475f
Jhomar 'Joe' Nando <contato@joekyy.com.br>
2024-01-11 20:31:07 -0300

    fix: scroll behavior

--- HTML/app/assets/js/jquery.functions.js
+++ HTML/app/assets/js/jquery.functions.js
@@ -1,73 +1,96 @@
-$(function () {
-  function isMobileDevice() {
+  $(function () {
+    function isMobileDevice() {
       return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  }
+    }
 
-  function updateMenuLink(sectionId) {
+    function updateMenuLink(sectionId) {
       $('nav ul li').removeClass('selected');
       $('nav ul li').find('a[href="#' + sectionId + '"]').parent().addClass('selected');
-  }
+    }
 
-  function updateURL(sectionId) {
+    function updateURL(sectionId) {
       if(history.pushState) {
-          history.pushState(null, null, '#' + sectionId);
+        history.pushState(null, null, '#' + sectionId);
       } else {
-          location.hash = '#' + sectionId;
+        location.hash = '#' + sectionId;
       }
-  }
+    }
 
-  if (!isMobileDevice()) {
+    function scrollToSection(sectionId) {
+      const sections = document.querySelectorAll('section');
+      const targetSection = document.getElementById(sectionId);
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest',
+          inline: 'start'
+        });
+        updateMenuLink(sectionId);
+        updateURL(sectionId);
+
+        // Atualiza o índice da seção atual com base na seção
+        const sectionIndex = Array.prototype.indexOf.call(sections, targetSection);
+        currentSectionIndex = sectionIndex >= 0 ? sectionIndex : currentSectionIndex;
+      }
+    }
+
+    if (window.location.hash) {
+      const hash = window.location.hash.substring(1); // Remove o '#' do hash
+      scrollToSection(hash);
+    }
+
+    if (!isMobileDevice()) {
       const sections = document.querySelectorAll('section');
       let currentSectionIndex = 0;
       let isScrolling = false;
 
       $('nav ul li a').on('click', function(e) {
-          e.preventDefault();
-          const targetId = $(this).attr('href').replace('#', '');
-          const targetSection = document.getElementById(targetId);
+        e.preventDefault();
+        const targetId = $(this).attr('href').replace('#', '');
+        const targetSection = document.getElementById(targetId);
 
-          if (targetSection) {
-              targetSection.scrollIntoView({
-                  behavior: 'smooth',
-                  block: 'nearest',
-                  inline: 'start'
-              });
-              updateMenuLink(targetId);
-              updateURL(targetId);
+        if (targetSection) {
+          targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'start'
+          });
+          updateMenuLink(targetId);
+          updateURL(targetId);
 
-              // Atualiza o índice da seção atual com base no link clicado
-              const sectionIndex = Array.prototype.indexOf.call(sections, targetSection);
-              currentSectionIndex = sectionIndex >= 0 ? sectionIndex : currentSectionIndex;
-          }
+          // Atualiza o índice da seção atual com base no link clicado
+          const sectionIndex = Array.prototype.indexOf.call(sections, targetSection);
+          currentSectionIndex = sectionIndex >= 0 ? sectionIndex : currentSectionIndex;
+        }
       });
 
       const onWheelEvent = (event) => {
-          if (isScrolling) {
-              return;
-          }
+        if (isScrolling) {
+          return;
+        }
 
-          event.preventDefault();
-          isScrolling = true;
-          setTimeout(() => isScrolling = false, 1000);
+        event.preventDefault();
+        isScrolling = true;
+        setTimeout(() => isScrolling = false, 1000);
 
-          if (event.deltaY < 0 && currentSectionIndex > 0) {
-              currentSectionIndex--;
-          } else if (event.deltaY > 0 && currentSectionIndex < sections.length - 1) {
-              currentSectionIndex++;
-          }
+        if (event.deltaY < 0 && currentSectionIndex > 0) {
+          currentSectionIndex--;
+        } else if (event.deltaY > 0 && currentSectionIndex < sections.length - 1) {
+          currentSectionIndex++;
+        }
 
-          const targetSection = sections[currentSectionIndex];
-          if (targetSection) {
-              targetSection.scrollIntoView({
-                  behavior: 'smooth',
-                  block: 'nearest',
-                  inline: 'start'
-              });
-              updateMenuLink(targetSection.id);
-              updateURL(targetSection.id);
-          }
+        const targetSection = sections[currentSectionIndex];
+        if (targetSection) {
+          targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'nearest',
+            inline: 'start'
+          });
+          updateMenuLink(targetSection.id);
+          updateURL(targetSection.id);
+        }
       };
 
       document.addEventListener('wheel', onWheelEvent, { passive: false });
-  }
-});
+    }
+  });
