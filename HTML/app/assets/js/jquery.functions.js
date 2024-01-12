830f48a078c1e2d2c49d1977acd01c46406e676c
Jhomar 'Joe' Nando <contato@joekyy.com.br>
2024-01-12 02:01:21 -0300

    feat: adding portifolio & client pages

--- HTML/app/assets/js/jquery.functions.js
+++ HTML/app/assets/js/jquery.functions.js
@@ -19,6 +19,107 @@
       }
     });
 
+    let currentSectionIndex = 0;
+    let isScrolling = false;
+    let isHorizontalScrollActive = false;
+    const sections = document.querySelectorAll('section');
+
+    if (window.location.hash) {
+      const hash = window.location.hash.substring(1);
+      scrollToSection(hash);
+    }
+
+    if (!isMobileDevice()) {
+      const onWheelEvent = (event) => {
+        if (isScrolling) {
+          return;
+        }
+
+        const targetSectionId = sections[currentSectionIndex].id;
+
+        if (targetSectionId === 'portifolio') {
+            isHorizontalScrollActive = true;
+            horizontalScroll(event);
+            return;
+        } else {
+            isHorizontalScrollActive = false;
+        }
+
+        if (!isHorizontalScrollActive) {
+          horizontalScrollPerSection(event)
+        }
+      };
+
+      document.addEventListener('wheel', onWheelEvent, { passive: false });
+    }
+
+    $('nav ul li a').on('click', function(e) {
+      e.preventDefault();
+      const targetId = $(this).attr('href').replace('#', '');
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest',
+          inline: 'start'
+        });
+        updateMenuLink(targetId);
+        updateURL(targetId);
+
+        const sectionIndex = Array.prototype.indexOf.call(sections, targetSection);
+        currentSectionIndex = sectionIndex >= 0 ? sectionIndex : currentSectionIndex;
+      }
+    });
+
+    function scrollToSection(sectionId) {
+      const sections = document.querySelectorAll('section');
+      const targetSection = document.getElementById(sectionId);
+      if (targetSection) {
+        targetSection.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest',
+          inline: 'start'
+        });
+        updateMenuLink(sectionId);
+        updateURL(sectionId);
+
+        const sectionIndex = Array.prototype.indexOf.call(sections, targetSection);
+        currentSectionIndex = sectionIndex >= 0 ? sectionIndex : currentSectionIndex;
+      }
+    }
+
+    function horizontalScroll(event) {
+      const element = document.querySelector("body");
+      event.preventDefault();
+      element.scrollBy({
+          left: event.deltaY < 0 ? -30 : 30
+      });
+    }
+
+    function horizontalScrollPerSection(event) {
+      event.preventDefault();
+          isScrolling = true;
+          setTimeout(() => isScrolling = false, 1000);
+
+          if (event.deltaY < 0 && currentSectionIndex > 0) {
+            currentSectionIndex--;
+          } else if (event.deltaY > 0 && currentSectionIndex < sections.length - 1) {
+            currentSectionIndex++;
+          }
+
+          const targetSection = sections[currentSectionIndex];
+          if (targetSection) {
+            targetSection.scrollIntoView({
+              behavior: 'smooth',
+              block: 'nearest',
+              inline: 'start'
+            });
+            updateMenuLink(targetSection.id);
+            updateURL(targetSection.id);
+          }
+    }
+
     function isMobileDevice() {
       return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     }
@@ -36,82 +137,4 @@
       }
     }
 
-    function scrollToSection(sectionId) {
-      const sections = document.querySelectorAll('section');
-      const targetSection = document.getElementById(sectionId);
-      if (targetSection) {
-        targetSection.scrollIntoView({
-          behavior: 'smooth',
-          block: 'nearest',
-          inline: 'start'
-        });
-        updateMenuLink(sectionId);
-        updateURL(sectionId);
-
-        // Atualiza o índice da seção atual com base na seção
-        const sectionIndex = Array.prototype.indexOf.call(sections, targetSection);
-        currentSectionIndex = sectionIndex >= 0 ? sectionIndex : currentSectionIndex;
-      }
-    }
-
-    if (window.location.hash) {
-      const hash = window.location.hash.substring(1); // Remove o '#' do hash
-      scrollToSection(hash);
-    }
-
-    const sections = document.querySelectorAll('section');
-    let currentSectionIndex = 0;
-    let isScrolling = false;
-
-    $('nav ul li a').on('click', function(e) {
-      e.preventDefault();
-      const targetId = $(this).attr('href').replace('#', '');
-      const targetSection = document.getElementById(targetId);
-
-      if (targetSection) {
-        targetSection.scrollIntoView({
-          behavior: 'smooth',
-          block: 'nearest',
-          inline: 'start'
-        });
-        updateMenuLink(targetId);
-        updateURL(targetId);
-
-        // Atualiza o índice da seção atual com base no link clicado
-        const sectionIndex = Array.prototype.indexOf.call(sections, targetSection);
-        currentSectionIndex = sectionIndex >= 0 ? sectionIndex : currentSectionIndex;
-      }
-    });
-
-    if (!isMobileDevice()) {
-
-      const onWheelEvent = (event) => {
-        if (isScrolling) {
-          return;
-        }
-
-        event.preventDefault();
-        isScrolling = true;
-        setTimeout(() => isScrolling = false, 1000);
-
-        if (event.deltaY < 0 && currentSectionIndex > 0) {
-          currentSectionIndex--;
-        } else if (event.deltaY > 0 && currentSectionIndex < sections.length - 1) {
-          currentSectionIndex++;
-        }
-
-        const targetSection = sections[currentSectionIndex];
-        if (targetSection) {
-          targetSection.scrollIntoView({
-            behavior: 'smooth',
-            block: 'nearest',
-            inline: 'start'
-          });
-          updateMenuLink(targetSection.id);
-          updateURL(targetSection.id);
-        }
-      };
-
-      document.addEventListener('wheel', onWheelEvent, { passive: false });
-    }
   });
