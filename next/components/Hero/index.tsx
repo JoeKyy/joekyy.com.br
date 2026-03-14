670181777656af672a3adb4807bae947f0831399
Jhomar Nando (Joe) <contato@joekyy.com.br>
2026-03-14 00:52:49 -0300

    refactor: update Hero component layout and improve image dimensions for better responsiveness

--- next/components/Hero/index.tsx
+++ next/components/Hero/index.tsx
@@ -15,21 +15,27 @@ export function Hero({ locale }: HeroProps) {
       id="hello"
       className="min-w-screen min-h-dvh lg:h-dvh flex items-center pt-27.5 lg:pt-0 px-4 lg:px-0"
     >
+      {/* Inner div matches original: width: 100vw on desktop */}
       <div className="w-full lg:w-screen">
+        {/* Row mirrors Bootstrap col-lg-5 + col-lg with 15px column gutters */}
         <div className="flex flex-col lg:flex-row items-center">
-          <div className="lg:flex-1">
+          {/* Text column: col-lg-5 = 5/12 */}
+          <div className="lg:w-5/12 lg:px-[15px]">
             <h3
-              className="text-lg lg:text-3xl font-normal text-center lg:text-right leading-[0.9] my-4 lg:my-0 [&>strong]:font-bold"
+              className="text-lg lg:text-[48px] font-normal text-center lg:text-right leading-[0.9] my-4 lg:my-0 [&>strong]:font-bold"
               style={{ textShadow: "0px 4px 4px rgba(0,0,0,0.25)" }}
               dangerouslySetInnerHTML={{ __html: hero.html }}
             />
           </div>
-          <div className="lg:flex-1 text-center">
+
+          {/* Image column: col-lg = 7/12, avatar at natural size */}
+          <div className="lg:w-7/12 lg:px-[15px] text-center">
             <Image
               src="/images/avatar.png"
               alt={hero.avatarAlt}
-              width={300}
-              height={300}
+              width={501}
+              height={501}
+              className="max-w-full h-auto mx-auto"
               priority
             />
           </div>
